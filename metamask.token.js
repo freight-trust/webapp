f9a881bc4bf25c12ecc336b17714d6ad48ebbaf3
sam <sam@freighttrust.com>
2020-10-20 16:18:40 -0700

    feat(plugins): metamask and perfumejs

---
+++ metamask.token.js
@@ -0,0 +1,33 @@
+//
+//
+//
+import MetaMaskOnboarding from '@metamask/onboarding';
+
+const tokenAddress = '0x49230c43f3343a509af16196a0f6be4d49175c48';
+const tokenSymbol = 'EDI';
+const tokenDecimals = 18;
+const tokenImage = 'https://raw.githubusercontent.com/freight-trust/branding/master/logo_electric/Freight%20Trust%20%26%20Clearing%20Logo%40half.png';
+
+try {
+  // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+  const wasAdded = await ethereum.request({
+    method: 'wallet_watchAsset',
+    params: {
+      type: 'ERC20', // Initially only supports ERC20, but eventually more!
+      options: {
+        address: tokenAddress, // The address that the token is at.
+        symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+        decimals: tokenDecimals, // The number of decimals in the token
+        image: tokenImage, // A string url of the token logo
+      },
+    },
+  });
+
+  if (wasAdded) {
+    console.log('Thanks for your interest!');
+  } else {
+    console.log('Your loss!');
+  }
+} catch (error) {
+  console.log(error);
+}
---
+++ perfume.js
@@ -0,0 +1,61 @@
+const perfume = new Perfume({
+  analyticsTracker: options => {
+    const {
+      metricName,
+      data,
+      eventProperties,
+      navigatorInformation,
+      vitalsScore,
+    } = options;
+    switch (metricName) {
+      case 'navigationTiming':
+        if (data && data.timeToFirstByte) {
+          myAnalyticsTool.track('navigationTiming', data);
+        }
+        break;
+      case 'networkInformation':
+        if (data && data.effectiveType) {
+          myAnalyticsTool.track('networkInformation', data);
+        }
+        break;
+      case 'storageEstimate':
+        myAnalyticsTool.track('storageEstimate', data);
+        break;
+      case 'fp':
+        myAnalyticsTool.track('firstPaint', { duration: data });
+        break;
+      case 'fcp':
+        myAnalyticsTool.track('firstContentfulPaint', { duration: data });
+        break;
+      case 'fid':
+        myAnalyticsTool.track('firstInputDelay', { duration: data });
+        break;
+      case 'lcp':
+        myAnalyticsTool.track('largestContentfulPaint', { duration: data });
+        break;
+      case 'lcpFinal':
+        myAnalyticsTool.track('largestContentfulPaintFinal', {
+          duration: data,
+        });
+        break;
+      case 'cls':
+        myAnalyticsTool.track('cumulativeLayoutShift', { value: data });
+        break;
+      case 'clsFinal':
+        myAnalyticsTool.track('cumulativeLayoutShiftFinal', { value: data });
+        break;
+      case 'tbt':
+        myAnalyticsTool.track('totalBlockingTime', { duration: data });
+        break;
+      case 'tbt10S':
+        myAnalyticsTool.track('totalBlockingTime10S', { duration: data });
+        break;
+      case 'elPageTitle':
+        myAnalyticsTool.track('elementTimingPageTitle', { duration: data });
+        break;
+      default:
+        myAnalyticsTool.track(metricName, { duration: data });
+        break;
+    }
+  },
+});
